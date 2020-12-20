45381c2334a95d18f3e8c1ed890c0f921cb2f171
asha joseph <ashajo183@gmail.com>
2020-12-20 17:58:28 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/asha/exercise1.java
@@ -0,0 +1,12 @@
+package se.lexicon.asha;
+
+import java.util.Arrays;
+
+public class exercise1 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{11, 23, 39, 56, 28};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
